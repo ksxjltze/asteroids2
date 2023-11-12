753780a19aeb8beccaf26b03ecec4fbb065400cc
Lee Jia Keat <leejiakeat@gmail.com>
2023-11-13 00:22:21 +0800

    spin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 
 #[derive(Component)]
@@ -14,6 +16,9 @@ struct ListTimer(Timer);
 
 #[derive(Component)]
 struct GameCamera;
+
+#[derive(Component)]
+struct Player;
 
 pub struct WeaponsPlugin;
 impl Plugin for WeaponsPlugin {
@@ -37,22 +42,39 @@ fn list_weapons(time: Res<Time>, mut timer: ResMut<ListTimer>, query: Query<&Nam
     }
 }
 
+fn spin(mut query: Query<&mut Transform, With<Player>>, time: Res<Time>){
+    let mut player_transform = query.single_mut();
+
+    player_transform.rotate_axis(Vec3::new(0.0, 0.0, 1.0), PI / 180.0 * 30.0 * time.delta_seconds());
+}
+
 fn setup(mut commands: Commands) {
     commands.spawn((Camera2dBundle::default(), GameCamera));
-    commands.spawn((SpriteBundle {
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            scale: (Vec3::new(100.0, 100.0, 1.0)),
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: (Vec3::new(100.0, 100.0, 1.0)),
+                ..default()
+            },
+            sprite: Sprite {
+                color: Color::rgb(0.3, 0.3, 0.7),
+                ..default()
+            },
             ..default()
         },
-        sprite: Sprite { color: Color::rgb(0.3, 0.3, 0.7), ..default() },
-        ..default()
-    }));
+        Player,
+    ));
 }
 
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
         .add_plugins((DefaultPlugins, WeaponsPlugin))
         .add_systems(Startup, setup)
+        .add_systems(Update, spin)
         .run();
 }
---
+++ wasm-build.ps1
@@ -0,0 +1,1 @@
+wasm-bindgen --out-dir ./pkg/ --target web .\target\wasm32-unknown-unknown\release\asteroids2.wasm
