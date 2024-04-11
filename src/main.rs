be591f67c26ed2ee894f773044b70c46d43a2c54
Lee Jia Keat <leejiakeat@gmail.com>
2024-04-11 11:46:19 +0800

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,48 @@ struct GameCamera;
 #[derive(Component)]
 struct Player;
 
-fn player_controller(
-    mut query: Query<&mut Transform, With<Player>>,
-    time: Res<Time>,
-    keys: Res<Input<KeyCode>>,
-    q_windows: Query<&Window, With<PrimaryWindow>>,
-) {
-    let mut player_transform = query.single_mut();
-    let mut direction = Vec3::new(0.0, 0.0, 0.0);
-
-    let player_speed = 100.0;
-    let window = q_windows.single();
-
+fn get_player_direction(player_transform: &Transform, window: &Window) -> Option<Vec3> {
     if let Some(position) = window.cursor_position() {
         let world_position_x = position.x - window.width() / 2.0;
         let world_position_y = -(position.y - window.height() / 2.0);
 
+        let mut direction = Vec3::new(0.0, 0.0, 0.0);
         direction.x = world_position_x - player_transform.translation.x;
         direction.y = world_position_y - player_transform.translation.y;
 
         direction = direction.normalize();
-        
-        player_transform.rotation = Quat::from_rotation_arc(Vec3::Y, direction);
+
+        return Some(direction);
+    }
+
+    None
+}
+
+fn player_controller_system(
+    mut query: Query<&mut Transform, With<Player>>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    time: Res<Time>,
+    keys: Res<Input<KeyCode>>,
+) {
+    let mut player_transform = query.single_mut();
+    let window = q_windows.single();
+
+    let player_speed = 100.0;
+    let direction = get_player_direction(&player_transform, window);
+    let mut movement = Vec3::ZERO;
+
+    match direction {
+        Some(x) => {
+            player_transform.rotation = Quat::from_rotation_arc(Vec3::Y, x);
+            movement = x * player_speed * time.delta_seconds();
+        }
+        None => (),
     }
 
     if keys.just_pressed(KeyCode::Space) {}
 
     if keys.pressed(KeyCode::W) {
-        player_transform.translation =
-            player_transform.translation + (direction * player_speed * time.delta_seconds());
+        player_transform.translation = player_transform.translation + movement;
     }
 }
 
@@ -62,6 +75,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, player_controller)
+        .add_systems(Update, player_controller_system)
         .run();
 }
